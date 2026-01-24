22997c9246a6e4f87289128e6c512d513bc5ff6e
Vansh Singh Rajput <vansh.rajput0709@gmail.com>
2026-01-24 14:07:14 +0530

    Create functions_in_c.c

---
+++ functions_in_c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int max_of_four(int a,int b,int c,int d){
+    int max = (a > b)?((a > c)?((a > d) ? a : d):((c > d) ? c : d)):((b > c)?((b > d) ? b : d):((c > d) ? c : d));
+    return max;
+}
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    return 0;
+}
